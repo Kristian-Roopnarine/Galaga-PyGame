97404bbb40f594205a9a41c61cab19bb80b25482
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-11-12 20:22:26 -0500

    Created methods and attributes for super class, Characteristics.

---
+++ Characteristics.py
@@ -0,0 +1,21 @@
+class Characteristics:
+
+    def __init__(self,x,y,width,height,color,health):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.health = health
+
+    def shoot(self):
+        pass
+
+    def isHit(self):
+        pass
+
+    def getX(self):
+        pass
+
+    def getY(self):
+        pass
