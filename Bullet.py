0bd52a44888bf4457c35072ad5dbe6290b29ef1e
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2019-11-12 22:46:07 -0500

    Created Bullet class.

---
+++ Bullet.py
@@ -0,0 +1,17 @@
+class Bullet:
+
+    def __init__(self,x,y,width,height,color,step):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.step = step
+
+    def moveUp(self):
+        self.y -= self.step
+    
+    def moveDown(self):
+        self.y += self.step
+
+    
